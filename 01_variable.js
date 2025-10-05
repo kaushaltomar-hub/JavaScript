f2544d858f5f8b582646789a5d4aba24fc532d97
kaushaltomar-hub <kaushaltomar2712@gmail.com>
2025-10-05 12:52:41 +0530

    commit

---
+++ 01_variable.js
@@ -0,0 +1,16 @@
+const accountId = 144553
+let accountEmail = "Kaushalthakur@gmail.com"
+var accountPassword = "12345"
+accountCity = "Aligarh"
+let accountState;
+
+//accountId = 2
+accountEmail = "dfu.com"
+accountPassword = "34342"
+accountCity = " Bengaluru"
+console.log([accountEmail,accountId,accountPassword,accountCity,accountState]);
+/*
+prefer not to use var
+bcz of the issue in the block scope and functional
+*/
+console.log(accountId);
---
+++ 02_dataTypes.js
@@ -0,0 +1,25 @@
+// "Use strict"; // treat all JS code as newer version
+ //alert(3+3) //we are using node js not browser
+
+//  console.log(3 + 3); 
+//  console.log("Kaushal");
+
+
+ let name = "Kaushal"
+ let age = 19
+ let isLoggedIn = false
+ let state = null;
+
+
+ //number=>2 to power 53
+ //bigInt
+ //String => "" / ''
+ //boolean => true/false
+ //null => standalone value
+ //Undefined => 
+ //symbol => unique
+
+ //object
+
+ console.log(typeof age);
+ 
